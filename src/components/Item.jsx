1d4bd879ca47320ca987831490e4c629db592e43
Jan Wilhelm <wilhelm.jan92@gmail.com>
2022-07-16 21:34:37 +0200

    add remove function

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { markDone, removeTodo } from "../features/todoSlice";
 import "./Item.css";
 
 function Item({ todo }) {
+  const dispatch = useDispatch();
+
+  const handle = (type) => {
+    type === "done"
+      ? dispatch(markDone(todo.id))
+      : dispatch(removeTodo(todo.id));
+  };
+
   return (
     <div className="item">
-      <p className={todo.done ? "item-done" : ""}>{todo.item}</p>
+      {todo.done ? (
+        <i
+          className="fa-solid fa-square-check"
+          onClick={() => handle("done")}
+        ></i>
+      ) : (
+        <i className="fa-solid fa-square" onClick={() => handle("done")}></i>
+      )}
+      <p className={todo.done ? "item-done" : ""} id="i">
+        {todo.item}
+      </p>
+      <button>
+        <i className="fa-solid fa-trash" onClick={() => handle("remove")}></i>
+      </button>
     </div>
   );
 }
--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -11,8 +11,26 @@ const todoSlice = createSlice({
     addTodo: (state, action) => {
       state.todoList.push(action.payload);
     },
+
+    markDone: (state, action) => {
+      state.todoList.map((item) => {
+        if (action.payload === item.id) {
+          if (item.done === true) {
+            item.done = false;
+          } else {
+            item.done = true;
+          }
+        }
+      });
+    },
+
+    removeTodo: (state, action) => {
+      return {
+        todoList: state.todoList.filter((item) => item.id !== action.payload),
+      };
+    },
   },
 });
 
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, markDone, removeTodo } = todoSlice.actions;
 export default todoSlice.reducer;
