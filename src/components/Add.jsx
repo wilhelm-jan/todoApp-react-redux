9a7beda0481f672fadd7f85345b19f4e35e1c95d
Jan Wilhelm <wilhelm.jan92@gmail.com>
2022-07-16 20:20:58 +0200

    configured store and reducer

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,13 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./Add.css";
+import { addTodo } from "../features/todoSlice";
 
 function Add() {
-  const [content, setContent] = useState();
-  const add = () => {};
+  const [content, setContent] = useState("");
+  const [saving, setSaving] = useState(false);
+  const inputElement = useRef();
+  const dispatch = useDispatch();
+
+  const add = () => {
+    setSaving(true);
+    dispatch(
+      addTodo({
+        id: Math.floor(Math.random() * 100),
+        item: content,
+        done: false,
+      })
+    );
+
+    setTimeout(() => {
+      setSaving(false);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    inputElement.current.focus();
+  }, []);
 
   return (
     <div className="add">
       <input
+        placeholder={content & saving ? "SAVING..." : ""}
+        ref={inputElement}
         type="text"
         value={content}
         onChange={(e) => setContent(e.target.value)}
